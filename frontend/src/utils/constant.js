3c8f37bcf9eb2260a37227314dda5cd95948c3ce
Dara <97618988+dara-tech@users.noreply.github.com>
2024-10-16 18:43:41 +0700

    audit

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,29 +1,29 @@
-export const USER_API_END_POINT = "http://localhost:8000/api/v1/user";
-export const JOB_API_END_POINT = "http://localhost:8000/api/v1/job";
-export const APPLICATION_API_END_POINT = "http://localhost:8000/api/v1/application";
-export const COMPANY_API_END_POINT = "http://localhost:8000/api/v1/company";
-export const ADMIN_API_END_POINT = "http://localhost:8000/api/v1/admin";
-export const CHAT_API_ENDPOINT = "http://localhost:8000";
-export const RESUME_API_ENDPOINT = "http://localhost:8000/api/v1/user/resumes";
-export const BLOG_API_ENDPOINT = "http://localhost:8000/api/v1/blog";
-export const PAGE_API_ENDPOINT = "http://localhost:8000/api/v1/page";
-export const PRODUCT_API_ENDPOINT = "http://localhost:8000/api/v1/product";
-export const INITIAL_PAGE = 1;
-export const JOBS_PER_PAGE = 10;
+// export const USER_API_END_POINT = "http://localhost:8000/api/v1/user";
+// export const JOB_API_END_POINT = "http://localhost:8000/api/v1/job";
+// export const APPLICATION_API_END_POINT = "http://localhost:8000/api/v1/application";
+// export const COMPANY_API_END_POINT = "http://localhost:8000/api/v1/company";
+// export const ADMIN_API_END_POINT = "http://localhost:8000/api/v1/admin";
+// export const CHAT_API_ENDPOINT = "http://localhost:8000";
+// export const RESUME_API_ENDPOINT = "http://localhost:8000/api/v1/user/resumes";
+// export const BLOG_API_ENDPOINT = "http://localhost:8000/api/v1/blog";
+// export const PAGE_API_ENDPOINT = "http://localhost:8000/api/v1/page";
+// export const PRODUCT_API_ENDPOINT = "http://localhost:8000/api/v1/product";
+// export const INITIAL_PAGE = 1;
+// export const JOBS_PER_PAGE = 10;
 
 
 
 // src/utils/constant.js
 
-// export const USER_API_END_POINT = "https://job-portal-u3t0.onrender.com/api/v1/user";
-// export const JOB_API_END_POINT = "https://job-portal-u3t0.onrender.com/api/v1/job";
-// export const APPLICATION_API_END_POINT = "https://job-portal-u3t0.onrender.com/api/v1/application";
-// export const COMPANY_API_END_POINT = "https://job-portal-u3t0.onrender.com/api/v1/company";
-// export const ADMIN_API_END_POINT = "https://job-portal-u3t0.onrender.com/api/v1/admin";                           
-// export const CHAT_API_ENDPOINT = "https://job-portal-u3t0.onrender.com";
-// export const RESUME_API_ENDPOINT = "https://job-portal-u3t0.onrender.com/api/v1/user/resumes";
-// export const BLOG_API_ENDPOINT = "https://job-portal-u3t0.onrender.com/api/v1/blog";
-// export const PAGE_API_ENDPOINT = "https://job-portal-u3t0.onrender.com/api/v1/page";
-// export const PRODUCT_API_ENDPOINT = "https://job-portal-u3t0.onrender.com/api/v1/product";
-// export const INITIAL_PAGE = 1;
-// export const JOBS_PER_PAGE = 10;
+export const USER_API_END_POINT = "https://job-portal-u3t0.onrender.com/api/v1/user";
+export const JOB_API_END_POINT = "https://job-portal-u3t0.onrender.com/api/v1/job";
+export const APPLICATION_API_END_POINT = "https://job-portal-u3t0.onrender.com/api/v1/application";
+export const COMPANY_API_END_POINT = "https://job-portal-u3t0.onrender.com/api/v1/company";
+export const ADMIN_API_END_POINT = "https://job-portal-u3t0.onrender.com/api/v1/admin";                           
+export const CHAT_API_ENDPOINT = "https://job-portal-u3t0.onrender.com";
+export const RESUME_API_ENDPOINT = "https://job-portal-u3t0.onrender.com/api/v1/user/resumes";
+export const BLOG_API_ENDPOINT = "https://job-portal-u3t0.onrender.com/api/v1/blog";
+export const PAGE_API_ENDPOINT = "https://job-portal-u3t0.onrender.com/api/v1/page";
+export const PRODUCT_API_ENDPOINT = "https://job-portal-u3t0.onrender.com/api/v1/product";
+export const INITIAL_PAGE = 1;
+export const JOBS_PER_PAGE = 10;
