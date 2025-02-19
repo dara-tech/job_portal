dbbebf9ba65146d9358f687365d2645cd452b460
Dara <97618988+dara-tech@users.noreply.github.com>
2025-02-19 11:35:40 +0700

    audit

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+import tailwindcssAnimate from 'tailwindcss-animate';
 export default {
 	darkMode: ["class"],
 	content: [
@@ -81,6 +82,6 @@ export default {
 		}
 	  }
 	},
-	plugins: [require("tailwindcss-animate")],
+	plugins: [tailwindcssAnimate],
   }
   
