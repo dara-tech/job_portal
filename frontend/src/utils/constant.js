a554c699132100bbb2048bdd91aa5c72a1303e9e
Dara <97618988+dara-tech@users.noreply.github.com>
2025-02-19 11:40:43 +0700

    update server

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -15,15 +15,15 @@
 
 // src/utils/constant.js
 
-export const USER_API_END_POINT = "https://genz-aicg.onrender.com/api/v1/user";
-export const JOB_API_END_POINT = "https://genz-aicg.onrender.com/api/v1/job";
-export const APPLICATION_API_END_POINT = "https://genz-aicg.onrender.com/api/v1/application";
-export const COMPANY_API_END_POINT="https://genz-aicg.onrender.com/api/v1/company";
-export const ADMIN_API_END_POINT = "https://genz-aicg.onrender.com/api/v1/admin";                           
-export const CHAT_API_ENDPOINT = "https://genz-aicg.onrender.com";
-export const RESUME_API_ENDPOINT = "https://genz-aicg.onrender.com/api/v1/user/resumes";
-export const BLOG_API_ENDPOINT = "https://genz-aicg.onrender.com/api/v1/blog";
-export const PAGE_API_ENDPOINT = "https://genz-aicg.onrender.com/api/v1/page";
-export const PRODUCT_API_ENDPOINT = "https://genz-aicg.onrender.com/api/v1/product";
+export const USER_API_END_POINT = "https://geez-eodh.onrender.com/api/v1/user";
+export const JOB_API_END_POINT = "https://geez-eodh.onrender.com/api/v1/job";
+export const APPLICATION_API_END_POINT = "https://geez-eodh.onrender.com/api/v1/application";
+export const COMPANY_API_END_POINT="https://geez-eodh.onrender.com/api/v1/company";
+export const ADMIN_API_END_POINT = "https://geez-eodh.onrender.com/api/v1/admin";                           
+export const CHAT_API_ENDPOINT = "https://geez-eodh.onrender.com";
+export const RESUME_API_ENDPOINT = "https://geez-eodh.onrender.com/api/v1/user/resumes";
+export const BLOG_API_ENDPOINT = "https://geez-eodh.onrender.com/api/v1/blog";
+export const PAGE_API_ENDPOINT = "https://geez-eodh.onrender.com/api/v1/page";
+export const PRODUCT_API_ENDPOINT = "https://geez-eodh.onrender.com/api/v1/product";
 export const INITIAL_PAGE = 1;
 export const JOBS_PER_PAGE = 10;
